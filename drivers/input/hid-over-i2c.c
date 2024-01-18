a402a1c7578e26aaca37f8eff9098a3d6fa4a901
Jonas Schwöbel <jonasschwoebel@yahoo.de>
2024-01-18 17:04:54 +0100

    wip prototype hid driver

--- drivers/input/hid-over-i2c.c
+++ drivers/input/hid-over-i2c.c
@@ -17,6 +17,7 @@
 #include <i2c.h>
 #include <asm/gpio.h>
 #include <dm/device.h>
+#include <power/regulator.h>
 
 #include "hid.h"
 
@@ -45,9 +46,16 @@ struct hid_descriptor {
 struct hid_over_i2c_priv {
 	struct input_config *input;	/* The input layer */
 	struct gpio_desc irq_gpio;
+	struct gpio_desc detect_gpio;
 
 	uint8_t* input_report;
 	uint8_t* report_descriptor;
+
+
+	struct udevice *vdd_supply;	/* Main voltage regulator (Vcc)*/
+	struct udevice *vddio_supply;	/* IO voltage regulator (Vccq)*/
+
+
 
 	struct hid_descriptor hid_descriptor;
 };
@@ -101,6 +109,13 @@ static int get_hid_descriptor(struct udevice *dev, struct hid_descriptor* hid_de
 	// Verify the HID Descriptor
 	if (hid_descriptor->wHIDDescLength != I2C_HID_DESCRIPTOR_SIZE ||
 		hid_descriptor->bcdVersion != I2C_HID_VERSION) {
+		return -EPROTO;
+	}
+	if (hid_descriptor->wReportDescLength == 0 ||
+		hid_descriptor->wMaxInputLength == 0 ||
+		hid_descriptor->wMaxOutputLength == 0 ||
+		hid_descriptor->wVendorID == 0 ||
+		hid_descriptor->wProductID == 0) {
 		return -EPROTO;
 	}
 
@@ -162,41 +177,50 @@ static int get_report_descriptor(struct udevice *dev, uint8_t *report_descriptor
 struct input_report {
 	uint16_t length;
 	uint8_t report_id;
-	uint8_t* data;
+	uint8_t data[40];
 } __attribute__((packed));
 
 static int get_input_report(struct udevice *dev) {
 	struct hid_over_i2c_priv *priv = dev_get_priv(dev);
 	struct hid_descriptor *hid_descriptor = &priv->hid_descriptor;
+	struct dm_i2c_chip *chip = dev_get_parent_plat(dev);
+	struct i2c_msg i2c_msgs[2];
+	uint8_t buffer[40] = {0};
 	uint16_t read_len;
 	int ret;
 
-	printf("Get Input report\n");
-	ret = hid_i2c_read_reg(dev, hid_descriptor->wMaxInputLength, (u8*)&read_len, sizeof(read_len));
-	
-	if(ret)
-		return ret;
-	printf("Report Size to read: %d\n", read_len);
+	//printf("Get Input report\n");
 
-	ret = hid_i2c_read_reg(dev, hid_descriptor->wInputRegister, priv->input_report, read_len);
+	i2c_msgs[0].addr = chip->chip_addr;
+	i2c_msgs[0].flags = I2C_M_RD;
+	i2c_msgs[0].len = hid_descriptor->wMaxInputLength;
+	i2c_msgs[0].buf = priv->input_report;
+
+	ret = dm_i2c_xfer(dev, i2c_msgs, 1);
 	if(ret)
 		return ret;
 
 	// Verify Input Report
 	struct input_report *in_rep = (struct input_report*)priv->input_report;
-	printf("size: %d, id: %d", in_rep->length, in_rep->report_id);
+
+	// Ignore touchpad report for now.
+	// Only accept keyboard reportID
+	if (in_rep->report_id != 1) {
+		return -ENODATA;
+	}
+
+	/*
+	printf("size: %d, id: %d\n", in_rep->length, in_rep->report_id);
 	printf("Input Report data:\n");
-	for (int i = 0; i < read_len-3; i++) {
+	
+	for (int i = 0; i < in_rep->length; i++) {
 		if (i % 16 == 0 && i != 0) {
 			printf("\n");
 		}
 		printf("%02x ", in_rep->data[i]);
 	}
 	printf("\n");
-
-	if (in_rep->length != read_len) {
-		return -EPROTO;
-	}
+	*/
 
 	return 0;
 }
@@ -217,15 +241,36 @@ static int keys_check(struct input_config *input)
 		return -1;
 	}
 
-	printf("hid: time to fetch a report\n");
+	//printf("hid: time to fetch an input report\n");
 
 
 	// fetch input report
-	get_input_report(input->dev);
+	ret = get_input_report(input->dev);
+	if (ret)
+		return ret;
+
+	struct input_report *in_rep = (struct input_report*)priv->input_report;
 
 	// decode input report
-
+#define RSVRD 0
+	int map[] = {
+RSVRD, RSVRD, RSVRD, RSVRD, KEY_A, KEY_B, KEY_C, KEY_D, KEY_E, KEY_F, KEY_G, KEY_H, KEY_I, KEY_J, KEY_K, KEY_L,
+KEY_M, KEY_N, KEY_O, KEY_P, KEY_Q, KEY_R, KEY_S, KEY_T, KEY_U, KEY_V, KEY_W, KEY_X, KEY_Y, KEY_Z, KEY_1, KEY_2,
+KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_ENTER, KEY_ESC, KEY_BACKSPACE, KEY_TAB, KEY_SPACE
+	};
+	
+	int keycode[5];
+	int count = 0;
+	for (int i = 0; i < 5; i++) {
+		keycode[i] = map[in_rep->data[i+1]];
+		if (keycode[i] != 0) {
+			count++;
+		}
+		//printf("%d\n", keycode[i]);
+	}
+	//printf("count: %d\n", count);
 	// write to stdin
+	input_send_keycodes(input, keycode, count);
 
 	return 0;
 }
@@ -237,15 +282,83 @@ static int hid_over_i2c_probe(struct udevice *dev)
 	struct stdio_dev *sdev = &uc_priv->sdev;
 	struct input_config *input = &uc_priv->input;
 	int ret;
+	
+	ret = gpio_request_by_name(dev, "detect", 0, &priv->detect_gpio, GPIOD_IS_IN);
+	printf("gpio_request: %d\n", ret);
+	
+	while (1) {
+		ret = dm_gpio_get_value(&priv->detect_gpio);
+		printf("gpio_value: %d\n", ret);
+		if (ret > 0)
+			break;
+	}
+	
+	
+	ret = device_get_supply_regulator(dev, "vdd-supply",&priv->vdd_supply);
+	if (ret) {
+		printf("vdd not found\n");
+	} else {
+		printf("vdd found\n");
+	}
+	
+	ret = device_get_supply_regulator(dev, "vddio-supply",&priv->vddio_supply);
+	if (ret) {
+		printf("vddio not found\n");
+	} else {
+		printf("vddio found\n");
+	}
+	
+	if (priv->vddio_supply) {
+		printf("enable vddio\n");
+		int ret = regulator_set_enable_if_allowed(priv->vddio_supply,true);
 
-	ret = get_hid_descriptor(dev, &priv->hid_descriptor);
-	print_hid_descriptor(&priv->hid_descriptor); // TODO debug only
+		if (ret) {
+			printf("Error vddio supply : %d\n", ret);
+			return ret;
+		}
+	}
+
+	mdelay(50);
+	
+	if (priv->vdd_supply) {
+		printf("enable vdd\n");
+		int ret = regulator_set_enable_if_allowed(priv->vdd_supply,true);
+
+		if (ret) {
+			printf("Error vdd supply : %d\n", ret);
+			return ret;
+		}
+	}
+	mdelay(100);
+	
+	
+	
+	
+	
+	
+
+	int gpio_ret;
+	
+	ret = ~0;
+	while(ret) {
+		gpio_ret = dm_gpio_get_value(&priv->detect_gpio);
+		printf("gpio_detect: %d\n", gpio_ret);
+		ret = get_hid_descriptor(dev, &priv->hid_descriptor);
+		printf("hid: %d\n", ret);
+		print_hid_descriptor(&priv->hid_descriptor); // TODO debug only
+	}
+	
 	if (ret)
 		return ret;
 
 	// Alloc memory for reports
 	priv->report_descriptor = devm_kzalloc(dev, priv->hid_descriptor.wReportDescLength, 0);
 	priv->input_report = devm_kzalloc(dev, priv->hid_descriptor.wMaxInputLength, 0);
+	if (priv->input_report == NULL) {
+		printf("Memory was NOT allocated!\n");
+	} else {
+		printf("Allocated memory at %p\n", priv->input_report);
+	}
 
 	// Get Report Descriptor and print it.
 	// Eventually skip ReportDescriptor as we only need BootMode.
@@ -255,10 +368,13 @@ static int hid_over_i2c_probe(struct udevice *dev)
 
 	// Request GPIO
 	gpio_request_by_name(dev, "irq", 0, &priv->irq_gpio, GPIOD_IS_IN);
+	
+
+	
 
 	// Configure Input.
 	input_set_delays(input, 240, 30);
-	input_allow_repeats(input, false);
+	input_allow_repeats(input, true);
 	input_add_tables(input, false);
 
 	priv->input = input;
@@ -266,13 +382,11 @@ static int hid_over_i2c_probe(struct udevice *dev)
 	input->read_keys = keys_check;
 	strcpy(sdev->name, "hid-over-i2c");
 
-//	ret = input_stdio_register(sdev);
+	ret = input_stdio_register(sdev);
 	if (ret) {
 		debug("%s: input_stdio_register() failed\n", __func__);
 		return ret;
 	}
-
-	get_input_report(dev);
 
 	return 0;
 }
